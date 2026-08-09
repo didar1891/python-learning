64fec96ea76394b6f675ba80db0531609aa35efb
MD.Didarul Islam <islamdidar019@gmail.com>
2026-08-09 11:02:18 +0600

    Add files via upload
    Basic code.py

---
+++ numbers.py
@@ -0,0 +1,10 @@
+x=10
+y=3
+
+print('Addition:',x+y)
+print('Subraction:',x-y)
+print('Multiplication:',x*y)
+print('Division:',x/y)
+print('Floor division:',x//y)
+print('Modulus:',x%y)
+print('power:',x ** y)
