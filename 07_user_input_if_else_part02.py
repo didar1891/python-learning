53e881a4ffa7bcf28c765d28b94de9d7f1acf319
MD.Didarul Islam <islamdidar019@gmail.com>
2026-08-09 12:00:19 +0600

    Add files via upload
    Basic code

---
+++ 07_user_input_if_else_part02.py
@@ -0,0 +1,16 @@
+marks=int(input("Enter the marks:"))
+
+if marks>=80:
+    print("Grade A+")
+elif marks>=70:
+    print("Grade A")
+elif marks>=60:
+    print("Grade B")
+elif marks>=50:
+    print("Grade C")
+elif marks>=40:
+    print("Grade D")
+
+else:
+    print("Fail")
+
