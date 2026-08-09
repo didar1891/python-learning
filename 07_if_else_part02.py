d5c1091e53dd2ef4f7e472a76c0b24b468d551f8
MD.Didarul Islam <islamdidar019@gmail.com>
2026-08-09 11:56:29 +0600

    Add files via upload
    Basic code

---
+++ 07_if_else_part02.py
@@ -0,0 +1,16 @@
+marks=75
+
+if marks>=80:
+    print("Grade A+")
+elif marks>=70:
+    print("Grade A")
+elif marks>=60:
+    print("Grade B")
+elif marks>=50:
+    print("Grade C")
+elif marks>=40:
+    print("Grade D")
+
+else:
+    print("Fail")
+
